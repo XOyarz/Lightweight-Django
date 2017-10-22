0c1dbaee78bf1938202e717ed4c4e8e1ac23daa2
Xavier <xavier982@hotmail.com>
2017-10-22 15:31:50 +0200

    ENVIRON variables

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
 import sys
+import os
 
 from django.conf import settings 
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1:8000',).split(',')
+
 settings.configure(
-	DEBUG=True,
-	SECRET_KEY='thisisthesecretkey',
+	DEBUG=DEBUG,
+	SECRET_KEY=SECRET_KEY,
+	ALLOWED_HOSTS=ALLOWED_HOSTS,
 	ROOT_URLCONF=__name__,
 	MIDDLEWARE_CLASSES=(
 		'django.middleware.common.CommonMiddleware',
@@ -15,6 +23,7 @@ settings.configure(
 
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 
@@ -24,6 +33,8 @@ def index(request):
 urlpatterns = (
 	url(r'^$', index),
 )
+
+application = get_wsgi_application()
 
 if __name__ == "__main__":
 	from django.core.management import execute_from_command_line
